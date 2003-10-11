3e47c2b98811ac497649e86ef7b7e90d4d58d5a5
גאבור סבו - Gábor Szabó <gabor@pti.co.il>
2003-10-11 10:17:28 +0000

    Import of SZABGAB/Array-Unique-0.04 from CPAN.
    gitpan-cpan-distribution: Array-Unique
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         SZABGAB/Array-Unique-0.04.tar.gz
    gitpan-cpan-author:       SZABGAB
    gitpan-cpan-maturity:     released

--- Unique.pm
+++ Unique.pm
@@ -2,61 +2,163 @@ package Array::Unique;
 
 use 5.006;
 use strict;
-#use warnings;
+use warnings;
 use Carp;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
-sub TIEARRAY  { 
+# strips out any duplicate values (leaves in the firts occurance)
+# and removes all undef values.
+sub unique {
+    my $self = shift; # self or class
+#    if (ref $_[0] eq 'Array::Unique') {
+#	$self = shift;
+#	print "#AU\n";
+#    }
+#    print "# @_\n";
+    
+
+    my %seen;
+    my @unique = grep defined $_ && !$seen{$_}++ && $_, @_;
+    # based on the Cookbook and on suggestion by Jeff 'japhy' Pinyan
+}
+
+
+sub TIEARRAY {
     my $class = shift;
-    my $mode = shift;
+    my $self = {
+		array => [],
+		hash => {},
+		};
+    bless $self, $class;
+}
 
 
-    my $self;
+sub CLEAR     { 
+    my $self = shift;
+    $self->{array} = [];
+    $self->{hash} = {};
+}
 
-    $mode = 'Std' unless (defined $mode);
-    
-    if ($mode eq "IxHash") {
-	$self = bless [], $class;
+sub EXTEND {}
 
-	require Array::Unique::IxHash;
- 	our @ISA;
-	#unshift @ISA, "Array::Unique::IxHash";
-	@ISA = ("Array::Unique::IxHash");
-	#print "DEBUG: @ISA\n";
-	# using unshift instead of shift because I had problems
-	# with the test code that the previous ISA was not cleaned
-	# so I used the same mode for all the test.
-	# This is an open issue what to do with the @ISA so only
-	# the current instance of the module should get it and not
-	# the futuer instances.
-    }
-    
-    if ($mode eq 'Std') {
-	$self = bless [], $class;
+sub STORE {
+    my ($self, $index, $value) = @_;
+    $self->SPLICE($index, 1, $value);
+}
 
-	require Array::Unique::Std;
- 	our @ISA;
-	#unshift @ISA, "Array::Unique::Std";
-	@ISA = ("Array::Unique::Std");
-	#print "DEBUG: @ISA\n";
-    }
 
-#    if ($mode eq 'Quick') {
-#	$self = bless {}, $class;
-#	require Array::Unique::Quick;
-# 	our @ISA;
-#	@ISA = ("Array::Unique::Quick");
+
+sub FETCHSIZE { 
+    my $self = shift;
+    return scalar @{$self->{array}};
+}
+
+sub FETCH { 
+    my ($self, $index) = @_;
+    ${$self->{array}}[$index];
+}
+
+
+sub STORESIZE { 
+    my $self = shift;
+    my $size = shift;
+
+    # We cannot enlarge the array as the values would be undef
+
+    # But we can make it smaller
+#   if ($self->FETCHSIZE > $size) {
+#	$self->{->Splice($size);
 #    }
 
+    $#{$self->{array}} = $size-1;
+    return $size;
+}
 
-    if (defined $self) {
-	$self->_init;
-	return $self;
-    } else {
-	carp("Invalid mode: '$mode'");
-	return undef;
+sub SPLICE {
+    my $self = shift;
+    my $offset = shift;
+    my $length = shift;
+
+#=pod
+    # reset length value to positive (this is done by the normal splice too)
+    if (defined $length and $length < 0) {
+	#$length = @{$self->{array}} + $length;
+	$length += $self->FETCHSIZE - $offset;
     }
+
+    # reset offset to positive (this is done by the normal splice too)
+    if (defined $offset and $offset < 0) {
+	$offset += $self->FETCHSIZE;
+    }
+#=cut
+
+    if (defined $offset and
+	$offset > $self->FETCHSIZE) {
+	$offset = $self->FETCHSIZE;
+        # should give a warning like this: splice() offset past end of array
+	# if this was really a splice (and warning set) but no warning if this
+	# was an assignment to a high index.
+    }
+
+#    my @s = @{$self->{array}}[$offset..$offset+$length]; # the old values to be returned
+    my @original;
+#    if (defined $length) {
+	@original = $self->Splice($self->{array}, $offset, $length, @_);
+#    } elsif (defined $offset) {
+#	@original = $self->Splice($self->{array}, $offset);
+#    } else {
+#	@original = $self->Splice($self->{array});
+#    }
+
+    return @original;
+}
+
+
+
+sub PUSH {
+    my $self = shift;
+
+    $self->SPLICE($self->FETCHSIZE, 0, @_);
+#    while (my $value = shift) {
+#	$self->STORE($self->FETCHSIZE+1, $value);
+#    }
+    return $self->FETCHSIZE;
+}
+
+sub POP {
+    my $self = shift;
+    ($self->SPLICE(-1))[0];
+}
+
+sub SHIFT {
+    my $self = shift;
+#    #($self->{array})[0];
+    ($self->SPLICE(0,1))[0];
+}
+
+sub UNSHIFT {
+    my $self = shift;
+    $self->SPLICE(0,0,@_);
+}
+
+
+sub Splice {
+    my $self = shift;
+    my $a = shift;
+    my $offset = shift;
+    my $length = shift;
+
+    my @original;
+    if (defined $length) {
+	@original = splice(@$a, $offset, $length, @_);
+    } elsif (defined $offset) {
+	@original = splice(@$a, $offset);
+    } else {
+	@original = splice(@$a);
+    }
+    @$a = $self->unique(@$a);
+    return @original;
 }
 
 1;
@@ -70,57 +172,31 @@ Array::Unique - Tieable array that allows only unique values
 =head1 SYNOPSIS
 
  use Array::Unique;
- tie @a, Array::Unique;
- or
- tie @a, Array::Unique, 'Std';
- or
- tie @a, Array::Unique, 'IxHash';
+ tie @a, 'Array::Unique';
 
- Use @a as a regular arrray.
+ Use @a as a regular array.
 
 =head1 DESCRIPTION
 
- This package supplies two different implementation of
- the Unique Array tie construct.
+This package lets you create an array which will allow
+only one occurence of any value.
 
- In both cases you tie a regular array to this class and
- use your array as you did earlier but now if try to add
- a value to the array in any way ($a[3]='s'; or push or 
- unshift or splice) which value was already an element 
- of the array the array will discard one of the copies.
+In other words no matter how many times you put in 42
+it will keep only the first occurance and the rest will
+be droped.
 
- By this you have an array where the values are unique.
+You use the module via tie and once you tied your array to
+this module it will behave correctly.
 
- Uniqueness is checeked with the 'eq' operator so 
- among other things it is case sensitive.
+Uniqueness is checeked with the 'eq' operator so 
+among other things it is case sensitive.
 
- The two implementations are:
- Std    - the Standard version and the
-          Array::Unique::Std
- IxHash - a version using the Tie::IxHash module of
-          Gurusamy Sarathy
-          Array::Unique::IxHash;
-
- You can select which implementation you would like to
- use by providing its name in the tie construct.
- The default currently is the Standard implementation.
-
- The main differences between the two implementations
- are speed and memory usage. 
- The Standard version uses about the same amount of space 
- as a regular array but adding new values takes O(n) time 
- so filling in an originally empty array will take O(n^2) time.
-
- The IxHash implementation requires installing Tie::IxHash
- uses about 4 times more space than a regular array but
- it is should be faster than the the Standard implementation.
-
- The module does not allow undef as a value in the array.
+The module does not allow undef as a value in the array.
 
 =head1 EXAMPLES
 
  use Array::Unique;
- tie @a, Array::Unique;
+ tie @a, 'Array::Unique';
 
  @a = qw(a b c a d e f);
  push @a, qw(x b z);
@@ -132,7 +208,50 @@ Array::Unique - Tieable array that allows only unique values
  to make sure there is only one occurence of each item,
  you have several option:
 
+
 =over 4
+
+=item 1) using an array and extracting the unique elements later
+
+ There is good discussion about it in the Perl Cookbook of O'Reilly.
+ I have copied the solutions here, you can see further discussion in the
+ book.
+
+ ----------------------------------------
+ Extracting Unique Elements from a List (Section 4.6 in the Perl Cookbook 1st ed.)
+
+ # Straightforward
+
+ %seen = ();
+ @uniq = ();
+ foreach $item (@list) [
+     unless ($seen{$item}) {
+       # if we get here we have not seen it before
+       $seen{$item} = 1;
+       push (@uniq, $item);
+    }
+ } 
+
+ # Faster
+ %seen = ();
+ foreach $item (@list) {
+   push(@uniq, $item) unless $seen{$item}++;
+ }
+
+ # Faster but different
+ %seen;
+ foreach $item (@list) {
+   $seen{$item}++;
+ }
+ @uniq = keys %seen;
+
+ # Faster and even more different
+ %seen;
+ @uniq = grep {! $seen{$_}++} @list;
+
+ ----------------------------------------
+
+ Anyway, all these solutions
 
 =item 1) using hash
 
@@ -196,8 +315,6 @@ Array::Unique - Tieable array that allows only unique values
  I think you cannot use two different implementations
  in the same script.
 
- Cannot use negative indexes in Splice (?)
-
 =head1 TODO
 
  Benchmark speed
@@ -215,7 +332,7 @@ Array::Unique - Tieable array that allows only unique values
 
  Gabor Szabo <gabor@perl.org.il>
 
- Copyright (C) 2002 Gabor Szabo <gabor@perl.org.il>
+ Copyright (C) 2002-2003 Gabor Szabo <gabor@perl.org.il>
  All rights reserved.
 
  You may distribute under the terms of either the GNU 
@@ -230,7 +347,7 @@ Array::Unique - Tieable array that allows only unique values
  you can send bug reports directly to the author.
 
  To get other support answers you should use either
- the Hungarian Perl mailing list at perl@atom.hu
+ the Hungarian Perl mailing list at http://www.perl.org.hu/
  if you want to ask in Hungarian or the Israeli Perl 
  mailing list at http://www.perl.org.il/ in English.
 
@@ -240,10 +357,11 @@ Array::Unique - Tieable array that allows only unique values
  Szabo Balazs (dLux)
  Shlomo Yona
  Gaal Yahas
+ Jeff 'japhy' Pinyan
 
 =head1 VERSION
 
- Version: 0.03      
- Date:    2002.07.27
+ Version: 0.04
+ Date:    2003.01.18
 
 =cut
